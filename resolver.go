277571e8797b04d259d8af0dee51bc5179bd6435
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-16 15:56:04 +0900

    :sparkles:ADD GQLgen init
    ~

---
+++ resolver.go
@@ -0,0 +1,26 @@
+package tam_project_server
+
+import (
+	"context"
+) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+
+type Resolver struct{}
+
+func (r *Resolver) Mutation() MutationResolver {
+	return &mutationResolver{r}
+}
+func (r *Resolver) Query() QueryResolver {
+	return &queryResolver{r}
+}
+
+type mutationResolver struct{ *Resolver }
+
+func (r *mutationResolver) PostMessage(ctx context.Context, text string, roomName string) (*Message, error) {
+	panic("not implemented")
+}
+
+type queryResolver struct{ *Resolver }
+
+func (r *queryResolver) Room(ctx context.Context, name string) (*Chatroom, error) {
+	panic("not implemented")
+}
