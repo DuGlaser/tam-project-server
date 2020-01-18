8e2495ed20994c775e05ed6861b49fd726ceab45
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-18 19:55:10 +0900

    :construction:[WIP]ADD postMessage process
    ~

--- resolver.go
+++ resolver.go
@@ -2,7 +2,12 @@ package tam_project_server
 
 import (
 	"context"
-) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+
+	detabase "github.com/DuGlaser/tam-project-server/database"
+	"github.com/DuGlaser/tam-project-server/models"
+)
+
+// THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 
 type Resolver struct{}
 
@@ -15,15 +20,30 @@ func (r *Resolver) Query() QueryResolver {
 
 type mutationResolver struct{ *Resolver }
 
-func (r *mutationResolver) PostMessage(ctx context.Context, text string, roomName string) (*Message, error) {
-	panic("not implemented")
+func (r *mutationResolver) PostMessage(ctx context.Context, text string, roomName string) (*models.Message, error) {
+	db := detabase.FetchConnection()
+	defer db.Close()
+
+	message := &models.Message{
+		Text: text,
+		ID:   1, /*TODO:randomなintの数*/
+	}
+
+	db.Create(message)
+
+	return message, nil
 }
 
 type queryResolver struct{ *Resolver }
 
-func (r *queryResolver) Room(ctx context.Context, name string) (*Chatroom, error) {
-	panic("not implemented")
+func (r *queryResolver) Room(ctx context.Context, name string) (*models.Chatroom, error) {
+	var chatroom *models.Chatroom
+	//TODO:DBと接続して情報を取得
+	return chatroom, nil
 }
-func (r *queryResolver) Rooms(ctx context.Context) (*Chatroom, error) {
-	panic("not implemented")
+func (r *queryResolver) Rooms(ctx context.Context) (*models.Chatroom, error) {
+	var chatroom *models.Chatroom
+	//TODO:DBと接続して情報を取得
+
+	return chatroom, nil
 }
